cc9617773150a4d32eca6d4d0c9265bf63cf7c71
Erwin Abu Bakar Sidik <64487987+erwinabs@users.noreply.github.com>
2024-01-16 09:39:51 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_10"
+export DT_LINK="https://github.com/erwinabs/twrp_realme_RMX2103"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="RMX2103"
+export OEM="realme"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
